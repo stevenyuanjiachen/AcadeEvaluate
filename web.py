736b9b049dac027c4f4094a013433ec843e8294f
Steven Yuan <1085320540@qq.com>
2024-12-12 21:39:49 +0800

    refactor: modified the code structure

--- web.py
+++ web.py
@@ -24,41 +24,41 @@ class AcadeEvaluateWeb:
         version = config["model"]["model_version"]
 
         # init the webset
+        self.searched_result = []
         self.web = gr.Blocks()
         with self.web:
             # TextBox
             self.paper_name_input = gr.Textbox(
                 label="请输入想要查询的论文", placeholder="在这里输入论文名称..."
             )
-            self.paper_recommend_output = gr.Textbox(label="查询到的论文")
-            self.paper_id_input = gr.Textbox(
-                label="请输入论文ID", placeholder="在这里输入Id...", visible=False
+            self.find_btn = gr.Button("查询")
+            self.paper_recommend_output = gr.Radio(
+                label="查询到的论文", interactive=True, choices=[]
             )
             self.evaluate_output = gr.Textbox(label="评价结果", visible=False)
 
-            # Buttons
-            with gr.Row():
-                self.find_btn = gr.Button("查询")
-                self.clear_btn = gr.Button("清除")
-                self.confirm_btn = gr.Button("确认论文", visible=False)
-            
+            user_choice_output = gr.Textbox(label="Your Choice", interactive=False)
+            self.confirm_btn = gr.Button("确认论文", visible=False)
+
             # Find button
             self.find_btn.click(
                 fn=self.find_clicked_event,
                 inputs=self.paper_name_input,
                 outputs=[
                     self.paper_recommend_output,
-                    self.find_btn,
-                    self.paper_id_input,
+                    # self.find_btn,
+                    # self.paper_id_input,
                     self.confirm_btn,
                 ],
             )
-
+            self.paper_recommend_output.change(
+                fn=self.display_choice,
+                inputs=self.paper_recommend_output,
+                outputs=user_choice_output,
+            )
             # Confirm button
             self.confirm_btn.click(
-                fn=self.confirm_clicked_event,
-                inputs=self.paper_id_input,
-                outputs=self.evaluate_output
+                fn=self.confirm_clicked_event, inputs=None, outputs=self.evaluate_output
             )
             self.confirm_btn.click(
                 fn=self.show_TextBox,
@@ -75,26 +75,34 @@ class AcadeEvaluateWeb:
             papers_list.append(
                 f"{idx}. {paper['title']}\n\tAuthors: {', '.join(author['name'] for author in paper['authors'])}"
             )
-
+        article_title = []
+        for i in range(len(self.papers)):
+            article_title.append(f"{self.papers[i]['title']}")
         # show the id input and confirm button
         return (
-            self.format_list(papers_list),
-            gr.update(visible=False),   # find_btn
-            gr.update(visible=True),    # paper_id_input
-            gr.update(visible=True),    # confirm_btn
+            gr.update(choices=article_title),
+            # gr.update(visible=False),   # find_btn
+            # gr.update(visible=True),    # paper_id_input
+            gr.update(visible=True),  # confirm_btn
         )
 
-    def confirm_clicked_event(self, paper_id: str):
-        paper = self.papers[int(paper_id)]
+    def display_choice(self, selected_article):
+        self.id = 0
+        for id, article in enumerate(self.papers):
+            if selected_article == article["title"]:
+                self.id = id
+        return f"You selected: {selected_article},id:{self.id}"
+
+    def confirm_clicked_event(self):
+        paper = self.papers[int(self.id)]
         self.download_citations(paper)
-        
 
     def show_TextBox(self):
         return gr.update(visible=True)
 
-    def format_list(self, list):
-        formatted_list = "\n".join([f"{line}" for line in list])
-        return formatted_list
+    # def format_list(self, list):
+    #     formatted_list = "\n".join([f"{line}" for line in list])
+    #     return formatted_list
 
     def download_citations(self, paper):
         # get the paper's citation
@@ -111,10 +119,12 @@ class AcadeEvaluateWeb:
             os.remove(os.path.join(dir, f))
 
         citationsDf = pd.DataFrame(citations)
-        citationsDf.to_csv(os.path.join(dir, "citations.csv"))  # 所有引用该文献的论文的信息
+        citationsDf.to_csv(
+            os.path.join(dir, "citations.csv")
+        )  # 所有引用该文献的论文的信息
         pdfURLs = citationsDf.dropna(subset=["openAccessPdf"])["openAccessPdf"].tolist()
         urlDf = pd.DataFrame(pdfURLs)
-        urlDf.to_csv(os.path.join(dir ,"pdfURLs.csv"))  # 有公开链接的文献的pdf链接
+        urlDf.to_csv(os.path.join(dir, "pdfURLs.csv"))  # 有公开链接的文献的pdf链接
         citationURLs = urlDf["url"].tolist()
 
         for result in download_papers_from_urls(
@@ -125,8 +135,9 @@ class AcadeEvaluateWeb:
                 print(f"{idx+1}/{len(citationURLs)} Error downloading {url}: {error}")
             else:
                 print(f"{idx+1}/{len(citationURLs)} Downloaded {url} to {filepath}")
-        
+
     def evaluate_paper(self, paper):
         print(paper)
+
 
 AcadeEvaluateWeb()
